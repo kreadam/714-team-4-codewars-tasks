ba198952b1cd4ba98e6d33730d5f48bcb186fb5d
sergey-maslov <shift.tfihs@gmail.com>
2022-09-20 19:18:50 +0000

    My solutions added

---
+++ maslov.js
@@ -0,0 +1,43 @@
+// https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+    if (num < 0) {
+      return num;
+    } else if (num >= 0) {
+      return num * (-1);
+    }
+  }
+
+  // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+  function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+
+  // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+  function move (position, roll) {
+    return position + roll * 2;
+  }
+
+  // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+
+  function greet (name, owner) {
+    if (name == owner) {
+      return "Hello boss";
+    } else {
+      return "Hello guest";
+    }
+  }
+
+  // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+
+  function litres(time) {
+    return Math.floor(time * 0.5);
+  }
+
+  // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+
+  function lovefunc(flower1, flower2){
+    return (flower1 % 2 == 0 && flower2 % 2 == 1) || (flower2 % 2 == 0 && flower1 % 2 == 1);
+  }
