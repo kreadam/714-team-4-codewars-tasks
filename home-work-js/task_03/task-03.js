eeb13a6a3caf13a65b572edf0464859e50624af9
kreadam <31982028+kreadam@users.noreply.github.com>
2022-09-26 16:16:48 +0300

    Update task-03.js

--- home-work-js/task_03/task-03.js
+++ home-work-js/task_03/task-03.js
@@ -1,18 +1,6 @@
 https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
 class SmallestIntegerFinder {
   findSmallestInt(args) {
-    let min = args[0];
-    for (let i = 1; i < args.length; i++){
-      if(args[i] < min){
-        min = args[i];
-      }
-    }
-    return min;
-  }
-}
-
-class SmallestIntegerFinder {
-  findSmallestInt(args) {
     return Math.min(...args);
   }
 }
@@ -23,19 +11,6 @@ function circleCircumference(circle) {
 }
 
 https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
-function giveMeFive(obj){
-  let arr = [];
-  for (var key in obj){
-    if(key.length === 5){
-      arr.push(key);
-    }
-    if(obj[key].length === 5){
-      arr.push(obj[key]);
-    }
-  }
-  return arr
-}
-
 function giveMeFive(obj){
   const five = [];
   for (var key in obj){
