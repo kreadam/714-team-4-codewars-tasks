d26eba399f8bb8c50d03eb7dd085c67bfb1b9aba
kreadam <31982028+kreadam@users.noreply.github.com>
2022-09-26 19:36:39 +0300

    Update task-03.js

--- home-work-js/task_03/task-03.js
+++ home-work-js/task_03/task-03.js
@@ -31,3 +31,37 @@ function buildFun(n) {
   return res;
 }
 
+https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age);
+    this.status = status;
+    this.legs = 0;
+    this.species = "shark";
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age);
+    this.status = status;
+    this.legs = 4;
+    this.species = "cat";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`
+  }
+}
+
+class Dog extends Animal {
+    constructor(name, age, status, master) {
+    super(name, age);
+    this.status = status;
+    this.legs = 4;
+    this.species = "dog";
+    this.master = master;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`
+  }
+}
